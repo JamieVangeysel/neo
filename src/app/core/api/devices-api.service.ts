d61c54526cb6e363bf237ca32475786d21c686b1
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2023-09-21 10:47:59 +0200

    add method to get all data

--- src/app/core/api/devices-api.service.ts
+++ src/app/core/api/devices-api.service.ts
@@ -10,7 +10,9 @@ export class DevicesApiService {
   constructor(private http: HttpClient) { }
 
   // https://api.jamievangeysel.be/v1/neo/devices/dac7ca013a360dd79f138b620275032c172715a254f0825d30c4cf77f9b38c6d4606b06549fb0a5cff1e522c007cb46e/data
-  getData<T>(device_uuid: string): Promise<IBaseApiResponse<T>> {
-    return firstValueFrom(this.http.get<IBaseApiResponse<T>>(`https://api.jamievangeysel.be/v1/neo/devices/${device_uuid}/data`))
+  getData<T>(device_uuid?: string): Promise<IBaseApiResponse<T>> {
+    if (device_uuid)
+      return firstValueFrom(this.http.get<IBaseApiResponse<T>>(`https://api.jamievangeysel.be/v1/neo/devices/${device_uuid}/data`))
+    return firstValueFrom(this.http.get<IBaseApiResponse<T>>(`https://api.jamievangeysel.be/v1/neo/devices/data`))
   }
 }
--- src/app/core/api/index.ts
+++ src/app/core/api/index.ts
@@ -13,13 +13,11 @@ export interface IBaseApiResponse<T> {
 export const trimParameters = (params: any): any => {
   for (const prop in params) {
     // ignore parameters that are set to null or are undefined
-    if (params[prop] === undefined || params[prop] === null) {
+    if (params[prop] === undefined || params[prop] === null)
       delete params[prop]
-    }
     // If the parameter contains the keyword id but is set to 0, ignore the parameter
-    if (prop.toLowerCase().indexOf('id') > -1 && params[prop] === 0) {
+    if (prop.toLowerCase().indexOf('id') > -1 && params[prop] === 0)
       delete params[prop]
-    }
   }
   return params
 }
